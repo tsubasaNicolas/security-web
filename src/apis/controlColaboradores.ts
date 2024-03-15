be60ce45277e540e1a911e3e5129457298d40d6f
tsubasaNicolás <36429291+tsubasaNicolas@users.noreply.github.com>
2024-03-14 23:47:06 -0300

    Update controlColaboradores.ts

--- src/apis/controlColaboradores.ts
+++ src/apis/controlColaboradores.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { IEstadoColaborador } from "../interfaces/IColaborador";
 
-const url = "https://backend-security.herokuapp.com/controlingreso";
+const url = "https://backend-security-production.up.railway.app/controlingreso";
 
 export const getRegistrosEstadoColaboradores = async () =>
   await axios.get(url + "/colaboradores");
